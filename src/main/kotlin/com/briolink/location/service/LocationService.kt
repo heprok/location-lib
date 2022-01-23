a6dc1d59407e4789da958c8928684a11e74debe7
Oleg Kravchuk <oleg@briolink.com>
2022-01-23 16:50:57 +0200

    BNM-548 Added service. Methods getLocationInfo, getSuggestionLocations

--- src/main/kotlin/com/briolink/location/service/LocationService.kt
+++ src/main/kotlin/com/briolink/location/service/LocationService.kt
@@ -1,104 +1,30 @@
 package com.briolink.location.service
 
+import com.briolink.location.model.ListLocationSuggestion
+import com.briolink.location.model.LocationId
+import com.briolink.location.model.LocationInfo
+import com.briolink.location.model.LocationSuggestion
+import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.client.WebClient
 
 class LocationService(private val webClient: WebClient) {
+    private val locationUrl = "locations"
+    fun getSuggestionLocations(query: String? = null): List<LocationSuggestion>? {
+        val listSuggestion = webClient.get()
+            .uri("/$locationUrl/?query=${query.orEmpty()}")
+            .accept(MediaType.APPLICATION_JSON)
+            .retrieve()
+            .bodyToMono(ListLocationSuggestion::class.java)
+            .block()
 
-//    fun suggestionLocations(
-//        query: String? = null,
-//        type: TypeLocationEnum? = null
-//    ): List<LocationSuggestion> {
-//        val userPermissionRole = webClient.get()
-//            .uri("/$permissionRoleUrl/?accessObjectId=$accessObjectId&accessObjectType=${accessObjectType.name}&userId=$userId")
-//            .retrieve()
-// //            .onStatus(HttpStatus::is4xxClientError) {
-// //                it.bodyToMono(ErrorResponseDto::class.java)
-// //            }
-// //            .onStatus(HttpStatus::is4xxClientError) { _ ->
-// //                Mono.error<PermissionRoleNotFoundException>(PermissionRoleNotFoundException(userId, accessObjectId))
-// //            }
-//            .bodyToMono(UserPermissionRoleDto::class.java)
-//            .block()
-//
-//        return userPermissionRole?.let { PermissionRoleEnum.ofId(it.role.id) }
-//    }
-//
-//    fun getUserPermissionRights(
-//        userId: UUID,
-//        accessObjectId: UUID,
-//        accessObjectType: AccessObjectTypeEnum
-//    ): UserPermissionRights? {
-//        val listUserPermissionRightsDto = webClient.get()
-//            .uri("/$permissionRightUrl/?accessObjectId=$accessObjectId&accessObjectType=${accessObjectType.name}&userId=$userId")
-//            .accept(MediaType.APPLICATION_JSON)
-//            .retrieve()
-//            .bodyToMono(ListUserPermissionRightDto::class.java)
-//            .block()
-//
-//        return listUserPermissionRightsDto?.let {
-//            UserPermissionRights(
-//                permissionRole = listUserPermissionRightsDto.userRole,
-//                permissionRights = listUserPermissionRightsDto.rights,
-//            )
-//        }
-//    }
-//
-//    fun isHavePermission(
-//        userId: UUID,
-//        accessObjectType: AccessObjectTypeEnum,
-//        accessObjectId: UUID,
-//        permissionRight: PermissionRightEnum
-//    ): Boolean {
-//        val isHavePermission = webClient.get()
-//            .uri("/$permissionRightUrl/check-permission/?accessObjectId=$accessObjectId&accessObjectType=${accessObjectType.name}&userId=$userId&permissionRight=${permissionRight.name}")
-//            .retrieve()
-//            .bodyToMono(Boolean::class.java)
-//            .block()
-//
-//        return isHavePermission ?: false
-//    }
-//
-//    @Throws(PermissionRoleExistException::class)
-//    fun createPermissionRole(
-//        userId: UUID,
-//        accessObjectType: AccessObjectTypeEnum,
-//        accessObjectId: UUID,
-//        permissionRole: PermissionRoleEnum
-//    ): UserPermissionRole? {
-//        val userPermissionRoleDto = webClient.post()
-//            .uri("/$permissionRoleUrl/?accessObjectId=$accessObjectId&accessObjectType=${accessObjectType.name}&userId=$userId&permissionRole=${permissionRole.name}")
-//            .retrieve()
-//            .onStatus({ it == HttpStatus.CONFLICT }, { throw PermissionRoleExistException() })
-//            .bodyToMono(UserPermissionRoleDto::class.java)
-//            .block()
-//
-//        return userPermissionRoleDto?.let { UserPermissionRole.fromDto(it) }
-//    }
-//
-//    fun editPermissionRole(
-//        userId: UUID,
-//        accessObjectType: AccessObjectTypeEnum,
-//        accessObjectId: UUID,
-//        permissionRole: PermissionRoleEnum
-//    ): UserPermissionRole? {
-//        val userPermissionRoleDto = webClient.put()
-//            .uri("/$permissionRoleUrl/?accessObjectId=$accessObjectId&accessObjectType=${accessObjectType.name}&userId=$userId&permissionRole=${permissionRole.name}")
-//            .retrieve().onStatus({ it == HttpStatus.NO_CONTENT }, { throw PermissionRoleExistException() })
-//            .bodyToMono(UserPermissionRoleDto::class.java).block()
-//
-//        return userPermissionRoleDto?.let { UserPermissionRole.fromDto(it) }
-//    }
-//
-//    fun deletePermissionRole(
-//        userId: UUID,
-//        accessObjectType: AccessObjectTypeEnum,
-//        accessObjectId: UUID,
-//    ): Boolean {
-//        val isDeleted = webClient.delete()
-//            .uri("/$permissionRoleUrl/?accessObjectId=$accessObjectId&accessObjectType=${accessObjectType.name}&userId=$userId")
-//            .retrieve().onStatus({ it == HttpStatus.NO_CONTENT }, { throw PermissionRoleExistException() })
-//            .bodyToMono(Boolean::class.java).block()
-//
-//        return isDeleted ?: false
-//    }
+        return listSuggestion?.listLocationSuggestion?.ifEmpty { null }
+    }
+
+    fun getLocationInfo(locationId: LocationId): LocationInfo? {
+        return webClient.get()
+            .uri("/$locationUrl/info?type=${locationId.type.name}/${locationId.id}")
+            .retrieve()
+            .bodyToMono(LocationInfo::class.java)
+            .block()
+    }
 }
